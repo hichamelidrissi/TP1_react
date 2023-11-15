584b05d808275c5816c84bb5c26a7acfba790299
hichamelidrissi <hisham.el.idrissi.7@gmail.com>
2023-11-15 21:55:57 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Calculator from './calculator/calc';
+import './calculator/calc.css'
+
+function App() {
+  return (
+    <div className="App">
+      <Calculator />
+    </div>
+  );
+}
+
+export default App;
